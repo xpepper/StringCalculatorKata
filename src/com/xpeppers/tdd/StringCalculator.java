8dfee3abd3d9782f3ab08231244b30dd41d68075
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-10-23 21:34:21 +0200

    first commit

---
+++ src/com/xpeppers/tdd/StringCalculator.java
@@ -0,0 +1,42 @@
+package com.xpeppers.tdd;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+	public static int add(String stringOfNumbers) {
+		if (stringOfNumbers.isEmpty())
+			return 0;
+
+		String[] numbers = extractNumbersFrom(stringOfNumbers);
+
+		return sum(numbers);
+	}
+
+	private static String[] extractNumbersFrom(String stringOfNumbers) {
+		String delimiter = ",";
+		if (stringOfNumbers.startsWith("//")) {
+			String[] tokens = stringOfNumbers.split("\n", 2);
+			delimiter = extract_delimiter_from(tokens[0]);			
+			stringOfNumbers = tokens[1];		
+		}
+		return stringOfNumbers.replaceAll("\n", delimiter).split(delimiter);
+	}
+
+	private static String extract_delimiter_from(String delimiterSection) {
+		Pattern pattern = Pattern.compile("//\\[(.+)\\]");
+		Matcher matcher = pattern.matcher(delimiterSection);
+		matcher.matches();
+		return Pattern.quote(matcher.group(1));
+	}
+
+	private static int sum(String[] numbers) {
+		int sum = 0;
+		for (String eachNumber : numbers) {
+			sum += Integer.valueOf(eachNumber);
+		}
+		return sum;
+	}
+
+}
---
+++ test/com/xpeppers/tdd/StringCalculatorTest.java
@@ -0,0 +1,50 @@
+package com.xpeppers.tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void isZeroForAnEmptyString() {
+		verifyAdd(0, "");
+	}
+
+	@Test
+	public void isJustTheNumberForAStringWithJustTheNumber() {
+		verifyAdd(1, "1");
+	}
+
+	@Test
+	public void sumsTheTwoNumbersForAStringOfCommaSeparatedNumbers() {
+		verifyAdd(3, "1,2");
+	}
+	
+	@Test
+	public void sumsAnUnknownAmountOfCommaSeparatedNumbers() {
+		verifyAdd(1+2+3+4, "1,2,3,4");
+	}
+	
+	@Test
+	public void canHandleNewLinesAsNumberDelimiter() {
+		verifyAdd(1+2+3+4, "1\n2,3\n4");
+	}
+
+	// “//[delimiter]\n[numbers…]”
+	@Test
+	public void supportsDifferentDelimiters() throws Exception {
+		verifyAdd(1+2+3+4, "//[***]\n1***2***3***4");
+	}
+	
+
+	private void verifyAdd(int expected, String stringOfNumbers) {
+		int result = StringCalculator.add(stringOfNumbers);
+		assertEquals(expected, result);
+	}
+	
+
+}
